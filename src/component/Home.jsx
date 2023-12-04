eca87d794de4b27a404fcca708ac677d72841173
Ofodile Justin <justinofodile@gmail.com>
2023-12-04 09:10:24 +0100

    Another modification

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="max-w-4xl mx-auto pt-10">Home</div>;
+  return <div className="max-w-4xl mx-auto pt-10 shadow-lg">Home</div>;
 };
 
 export default Home;
